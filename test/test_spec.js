4944653d37228051414d1e0da4cfbb59c37acf3d
Matt Chamberlain <mchamber@thoughtworks.com>
2015-11-14 10:37:23 +0000

    @chambois: Basic test setup for React, Mocha, ES6

---
+++ test/test_spec.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import chai from 'chai';
+
+var expect = chai.expect;
+
+describe('Something', function() {
+
+  it('does something', () => {
+    expect(true).to.equal(true);
+  });
+
+});
+
